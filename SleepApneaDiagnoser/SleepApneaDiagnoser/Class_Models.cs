743844afe7c2ab34b3b22639ee95b3eef641a4d1
Kunal-Custom <Kunal-Custom@KUNAL>
2017-01-29 03:13:33 -0500

    Added member variables that are needed for EEG analysis.

--- SleepApneaDiagnoser/SleepApneaDiagnoser/Class_Models.cs
+++ SleepApneaDiagnoser/SleepApneaDiagnoser/Class_Models.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OxyPlot;
+using OxyPlot.Series;
+using MathWorks.MATLAB.NET.Arrays;
 
 namespace SleepApneaDiagnoser
 {
@@ -119,13 +121,17 @@ namespace SleepApneaDiagnoser
     /// </summary>
     public string EEGEDFSelectedSignal;
     /// <summary>
-    /// The user selected start time for the eeg analysis in 30s epochs
+    /// The user selected Number of Epochs for eeg analysis in 30s epochs
     /// </summary>
-    public int EEGEDFStartRecord;
+    public int EpochForAnalysis;
     /// <summary>
-    /// The user selected period for the eeg analysis in 30s epochs
+    /// The user selected start epoch for eeg analysis export in 30s epochs
     /// </summary>
-    public int EEGEDFDuration;
+    public int ExportEpochStart;
+    /// <summary>
+    /// The user selected end epoch for eeg analysis export in 30s epochs
+    /// </summary>
+    public int ExportEpochEnd;
     /// <summary>
     /// The eeg analysis plot to be displayed
     /// </summary>
@@ -149,6 +155,7 @@ namespace SleepApneaDiagnoser
     /// <summary>
     /// Displays the eeg spectrogram power plot
     /// </summary>
+    public MWNumericArray[] freqRange;
   }
 
   /// <summary>
