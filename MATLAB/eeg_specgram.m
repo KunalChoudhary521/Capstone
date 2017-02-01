2146965c3bb934dd19f66800178c388c8590ce0e
Kunal-Custom <Kunal-Custom@KUNAL>
2017-02-01 00:33:20 -0500

    Functional EEG Spectrogram MATLAB script (Working).

--- MATLAB/eeg_specgram.m
+++ MATLAB/eeg_specgram.m
@@ -1,4 +1,20 @@
-function [spec_mat, time] = eeg_specgram(signal, sample_freq)
-    [res,~,time] = spectrogram(signal,[],[],[],sample_freq);
-    spec_mat = abs(res);%calculate magnitude of complex number matrix
+function [spec_mat,f,t] = eeg_specgram(signal, sample_freq,varargin)
+    optional_args = length(varargin);    
+    switch optional_args
+        case 0
+            [spec_mat,f,t] = specgram(signal, sample_freq);
+        case 3
+            win = varargin{1}; n_overlap = varargin{2}; nfft = varargin{3};
+            [spec_mat,f,t] = specgram_window(signal, sample_freq,win,n_overlap, nfft);
+    end
+end
+
+function [spec_mat,f,t] = specgram(signal, sample_freq)
+    [C,f,t] = spectrogram(signal,[],[],[],sample_freq, 'yaxis');
+    spec_mat = 10 * log10(abs(C));%C refers to colour at frequency f & time t
+end
+
+function [spec_mat,f,t] = specgram_window(signal, sample_freq, win_size, n_overlap, nfft)
+    [C,f,t] = spectrogram(signal,win_size,n_overlap,nfft,sample_freq, 'yaxis');
+    spec_mat = 10 * log10(abs(C));%C refers to colour at frequency f & time t
 end
